e67d3bf7e619bb03a5c8b632043c3ae2bee79451
Tiffany <chirashii>
2019-04-06 18:12:11 -0700

    import material ui, change basic app

---
+++ js-app/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        <Typography component="h2" variant="h2" gutterBottom>Pictionary</Typography>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default App;
