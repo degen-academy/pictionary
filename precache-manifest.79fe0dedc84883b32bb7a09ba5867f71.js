70d8d7bcacf4a43d87c7a8671aa6968439e57747
Tiffany <chirashii>
2019-04-06 20:02:13 -0700

    Updates

---
+++ precache-manifest.79fe0dedc84883b32bb7a09ba5867f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8086e05b462ffa3f1afe",
+    "url": "/pictionary/static/css/main.bfcb6753.chunk.css"
+  },
+  {
+    "revision": "8086e05b462ffa3f1afe",
+    "url": "/pictionary/static/js/main.dc245981.chunk.js"
+  },
+  {
+    "revision": "7dab390b8a3ac02cd73e",
+    "url": "/pictionary/static/js/runtime~main.2977027c.js"
+  },
+  {
+    "revision": "2047dc531e120190fc90",
+    "url": "/pictionary/static/js/2.7d46646e.chunk.js"
+  },
+  {
+    "revision": "d36070558b55b51118f146816d590ae0",
+    "url": "/pictionary/index.html"
+  }
+];
