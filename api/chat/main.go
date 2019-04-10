b6a6447e490f1036ad58040b0658c57dde25ffde
tihuang <tiffany.huang08@gmail.com>
2019-04-10 02:03:38 -0700

    actually make the broadcast method work

--- api/chat/main.go
+++ api/chat/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -13,10 +14,14 @@ import (
 
 	// ag "github.com/aws/aws-sdk-go/service/apigatewaymanagementapi"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
 const tableName = "pictionary"
+
+var access string
+var secret string
 
 type Response events.APIGatewayProxyResponse
 
@@ -36,9 +41,11 @@ type Message struct {
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (Response, error) {
 	sess := session.New()
+	// fmt.Printf("sessino: %+v", sess.Config)
+
 	h := handler{
 		ddb: dynamodb.New(sess, aws.NewConfig().WithRegion("us-west-2")),
-		agw: apigatewaymanagementapi.New(sess),
+		agw: apigatewaymanagementapi.New(sess, aws.NewConfig().WithRegion("us-west-2")),
 	}
 	message := &Message{}
 	json.Unmarshal([]byte(req.Body), message)
@@ -46,7 +53,7 @@ func Handler(ctx context.Context, req events.APIGatewayWebsocketProxyRequest) (R
 	if message.Event == "join" {
 		return h.join(ctx, req, message)
 	} else if message.Event == "broadcast" {
-		return h.broadcast(ctx, req, message.Message)
+		return h.broadcast(ctx, req, message)
 	}
 
 	return Response{}, fmt.Errorf("connection handler: do not understand event type %s", req.RequestContext.EventType)
@@ -65,32 +72,62 @@ func (h *handler) join(ctx context.Context, req events.APIGatewayWebsocketProxyR
 			"Name": {
 				S: aws.String(message.Name),
 			},
+			"GameID": {
+				S: aws.String(message.GameID),
+			},
 		},
 		TableName: aws.String(tableName),
 	})
 
 	if err != nil {
-		return Response{}, fmt.Errorf("could not join game-id %s: %s", message.GameID, err)
+		return Response{
+			Body: err.Error(),
+		}, fmt.Errorf("could not join game-id %s: %s", message.GameID, err)
 	}
 
 	return Response{StatusCode: 200, Body: fmt.Sprintf("joined game id %s as %s", message.GameID, message.Name)}, nil
 }
 
-func (h *handler) broadcast(ctx context.Context, req events.APIGatewayWebsocketProxyRequest, message string) (Response, error) {
-	// connectionID := "XxoHKe2DvHcAdgQ="
+type connectionIDRecord struct {
+	SK string
+}
 
-	// // get connectionIDs
+func (h *handler) broadcast(ctx context.Context, req events.APIGatewayWebsocketProxyRequest, message *Message) (Response, error) {
+	res, err := h.ddb.Query(&dynamodb.QueryInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":v1": {
+				S: aws.String("game-id:connection-id"),
+			},
+			":gid": {
+				S: aws.String(message.GameID),
+			},
+		},
+		KeyConditionExpression: aws.String("PK = :v1 and begins_with(SK, :gid)"),
+		TableName:              aws.String(tableName),
+	})
 
-	// h.agw.ClientInfo.Endpoint = "https://0sn07huhd5.execute-api.us-west-2.amazonaws.com/prod"
-	// connectionsReq, _ := h.agw.PostToConnectionRequest(&apigatewaymanagementapi.PostToConnectionInput{
-	// 	ConnectionId: aws.String(connectionID),
-	// 	Data:         []byte("hello"),
-	// })
-	// connectionsReq.SetBufferBody([]byte(body.Message))
-	// err := connectionsReq.Send()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	if err != nil {
+		return Response{
+			Body: err.Error(),
+		}, fmt.Errorf("failed to broadcast to game-id %s: %s", message.GameID, err)
+	}
+
+	for _, connection := range res.Items {
+		var s string
+		dynamodbattribute.NewDecoder().Decode(connection["SK"], &s)
+
+		connectionID := strings.Split(s, ":")[1]
+		h.agw.ClientInfo.Endpoint = "https://noc8wkkta6.execute-api.us-west-2.amazonaws.com/prod"
+		connectionsReq, _ := h.agw.PostToConnectionRequest(&apigatewaymanagementapi.PostToConnectionInput{
+			ConnectionId: aws.String(connectionID),
+			Data:         []byte("hello"),
+		})
+		connectionsReq.SetBufferBody([]byte(message.Message))
+		err := connectionsReq.Send()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 
 	return Response{StatusCode: 200, Body: "broadcasted"}, nil
 }
