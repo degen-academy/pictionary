86922267ee79362e3157f2fa1b1df588b7a9bfbf
Tiffany <chirashii>
2019-04-27 02:29:01 -0700

    basic socket implementation

--- js-app/src/components/game/component.tsx
+++ js-app/src/components/game/component.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import { RouteComponentProps } from "react-router";
 import { string } from "prop-types";
+import { join } from "path";
 
 // hard-coded players
 const players = [
@@ -25,12 +26,27 @@ class GameLobby extends React.Component<Props> {
     state = {
         displayName: "",
     }
+    socket:WebSocket;
+    
     constructor(props: Props) {
         super(props);
 
-        this.state = {
-            displayName: this.props.location.state
+        this.state.displayName = this.props.location.state;
+
+        const socket = new WebSocket('wss://auxhu82pyl.execute-api.us-west-2.amazonaws.com/dev');
+        socket.onopen = () => {
+            const message = {
+                action: "join",
+                game_id: this.props.match.params.gameID,
+                name: this.state.displayName,
+            }
+            socket.send(JSON.stringify(message));
         }
+
+        socket.onmessage = (event) => {
+            console.log(event);
+        }
+        this.socket = socket;
 
     }
 
@@ -44,14 +60,38 @@ class GameLobby extends React.Component<Props> {
 
         return (
         <div>
+            <div>
             <h1>Game ID: {this.props.match.params.gameID}</h1>
             {message}
             <Button variant="contained" color="primary">
                 Start Game
             </Button>
+            <TextField
+            id="outlined-uncontrolled"
+            label="chat"
+            margin="normal"
+            variant="outlined"
+            />
+            </div>
+            <br />
+            <div>
+            <Button variant="contained" color="primary" onClick={this.sendMessage}>
+                Chat
+            </Button>
+            </div>
 
         </div>
         );
+    }
+
+    private sendMessage = () => {
+        const message = {
+            action: "send_message",
+            message: "hello",
+            game_id: this.props.match.params.gameID,
+            name: this.state.displayName
+        }
+        this.socket.send(JSON.stringify(message))
     }
 }
 
