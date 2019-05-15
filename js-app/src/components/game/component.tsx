33747a1608cc1b160071e2073d82184676774f66
Tiffany <chirashii>
2019-05-15 03:12:57 -0700

    super ugly - got more real time streaming

--- js-app/src/components/game/component.tsx
+++ js-app/src/components/game/component.tsx
@@ -6,212 +6,328 @@ import CanvasFreeDrawing from "canvas-free-drawing";
 
 declare var MediaRecorder: any;
 interface CanvasElement extends HTMLCanvasElement {
-    captureStream(int): any;
- }
-  
+  captureStream(int): any;
+}
 
 interface MatchParams {
   gameID: string;
 }
 
 interface Props extends RouteComponentProps<MatchParams> {
-    displayName: string;
+  displayName: string;
 }
 
 interface State {
-    displayName: string;
-    messageHistory: chatLine[];
+  displayName: string;
+  messageHistory: chatLine[];
 }
 
 interface chatLine {
-    name: string;
-    message: string;
+  name: string;
+  message: string;
 }
 
 class GameLobby extends React.Component<Props, State> {
-    myCanvas: HTMLCanvasElement | null;
-    myRecordingCanvasRef: React.RefObject<HTMLCanvasElement>;
-    myVideoElement: React.RefObject<HTMLVideoElement>;
-    myStream: MediaStream;
-    gameAPI: GameAPI;
-    chatInput: React.RefObject<HTMLInputElement>;
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            displayName: this.props.location.state,
-            messageHistory: []
-        }
-        this.gameAPI = new GameAPI({
-            gameID: this.props.match.params.gameID,
-            displayName: this.state.displayName,
-            onMessage: this.onReceiveMessage,
-        });
-        this.chatInput = React.createRef<HTMLInputElement>();
-        this.myCanvas = null;
-        this.myRecordingCanvasRef = React.createRef<HTMLCanvasElement>();
-        this.myVideoElement = React.createRef<HTMLVideoElement>();
+  myCanvas: HTMLCanvasElement | null;
+  myRecordingCanvasRef: React.RefObject<HTMLCanvasElement>;
+  myVideoElement: React.RefObject<HTMLVideoElement>;
+  myStream: MediaStream;
+  gameAPI: GameAPI;
+  chatInput: React.RefObject<HTMLInputElement>;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      displayName: this.props.location.state,
+      messageHistory: []
+    };
+    this.gameAPI = new GameAPI({
+      gameID: this.props.match.params.gameID,
+      displayName: this.state.displayName,
+      onMessage: this.onReceiveMessage
+    });
+    this.chatInput = React.createRef<HTMLInputElement>();
+    this.myCanvas = null;
+    this.myRecordingCanvasRef = React.createRef<HTMLCanvasElement>();
+    this.myVideoElement = React.createRef<HTMLVideoElement>();
+  }
 
+  render() {
+    console.log("rerender");
+    const displayName = this.state.displayName;
+    console.log(displayName);
+    if (!displayName) {
+      return <h2>Please log in</h2>;
     }
 
-    render() {
-        const displayName = this.state.displayName;
-        console.log(displayName)
-        if (!displayName) {
-            return <h2>Please log in</h2>
-        }
-
-        const message = <h2>Joined the lobby as "{displayName}"</h2>;
-        console.log(this.state.messageHistory)
-        const h = this.state.messageHistory.map(d => {
-           return <div><span>{d.name}:</span>  {d.message}</div>
-        })
-        console.log(h)
-
-        return (
+    const message = <h2>Joined the lobby as "{displayName}"</h2>;
+    console.log(this.state.messageHistory);
+    const h = this.state.messageHistory.map(d => {
+      return (
         <div>
-            <div>
-            <h1>Game ID: {this.props.match.params.gameID}</h1>
-            {message}
-
-            </div>
-            <br />
-            <div>
-            <TextField
-                inputRef={this.chatInput}
-                id="chat-input"
-                label="chat"
-                margin="normal"
-                variant="outlined"
-                onKeyDown={this.handleKeyDown}
-            />
-            <Button variant="contained" color="primary" onClick={this.sendMessage}>
-                Send
-            </Button>
-
-            <canvas id="cfd" style={{border: '1px solid black'}} ref={this.setCanvasRef}/>
-            <canvas id="cfd2" style={{border: '1px solid red'}} ref={this.myRecordingCanvasRef} />
-            <video style={{border: '1px solid blue', width: 500, height: 500}} ref={this.myVideoElement} />
-            {h}
-
-            </div>
-
+          <span>{d.name}:</span> {d.message}
         </div>
-        );
-    }
+      );
+    });
+    console.log(h);
 
-    private setCanvasRef = (el:HTMLCanvasElement) => {
-        this.myCanvas = el;
-        const cfd = new CanvasFreeDrawing({
-            elementId: 'cfd',
-            width: 500,
-            height: 500,
-          });
-        const cfd2 = new CanvasFreeDrawing({
-            elementId: 'cfd2',
-            width: 500,
-            height: 500,
-        });
-    
-        // set properties
-        cfd.setLineWidth(10); // in px
-        cfd.setStrokeColor([0, 0, 255]);
-        this.myStream =(el as CanvasElement).captureStream(30);
-        const recorder = new MediaRecorder(this.myStream, {mimeType: 'video/webm;codecs=vp8'});
-        const allChunks = [];
-      
-        const mediaSource = new MediaSource(); 
+    return (
+      <div>
+        <div>
+          <h1>Game ID: {this.props.match.params.gameID}</h1>
+          {message}
+        </div>
+        <br />
+        <div>
+          <TextField
+            inputRef={this.chatInput}
+            id="chat-input"
+            label="chat"
+            margin="normal"
+            variant="outlined"
+            onKeyDown={this.handleKeyDown}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={this.sendMessage}
+          >
+            Send
+          </Button>
 
-        let sbuf;
-        let updateend = 0;
-        let queue = [];
-        mediaSource.addEventListener('sourceopen', function(e) {
-            sbuf = mediaSource.addSourceBuffer('video/webm;codecs="vp8"');
-            sbuf.addEventListener('updateend', function(e) {
-                updateend++;
-                console.log(`update end: ${updateend}`)
-                console.log(mediaSource.readyState)
-                if (queue.length > 0) {
-                    sbuf.appendBuffer(queue.shift() as Uint8Array)
-                }
-            }, false);
-        }, false);
-       
+          <canvas
+            id="cfd"
+            style={{ border: "1px solid black" }}
+            ref={this.setCanvasRef}
+          />
+          <canvas
+            id="cfd2"
+            style={{ border: "1px solid red" }}
+            ref={this.myRecordingCanvasRef}
+          />
+          <video
+            id="video"
+            style={{ border: "1px solid blue", width: 500, height: 500 }}
+            ref={this.myVideoElement}
+          />
+          {h}
+        </div>
+      </div>
+    );
+  }
 
-        let ondata = 0;
+  private setCanvasRef = (el: HTMLCanvasElement) => {
+    this.myCanvas = el;
+    const cfd = new CanvasFreeDrawing({
+      elementId: "cfd",
+      width: 500,
+      height: 500
+    });
+    const cfd2 = new CanvasFreeDrawing({
+      elementId: "cfd2",
+      width: 500,
+      height: 500
+    });
 
-        const video = document.querySelector('video');
+    // set properties
+    cfd.setLineWidth(10); // in px
+    cfd.setStrokeColor([0, 0, 255]);
+    this.myStream = (el as CanvasElement).captureStream(60);
+    const recorder = new MediaRecorder(this.myStream, {
+      mimeType: 'video/webm;codecs="vp8"',
+      mode: "sequence"
+    });
+    const allChunks = [];
 
+    const mediaSource = new MediaSource();
 
-        const dataavailable = (e) => {
-
-        let fr = new FileReader();
-            fr.onloadend = function(e){
-                console.log(e)
-                let res = new Uint8Array(fr.result as ArrayBuffer);
-                if (sbuf.updating) {
-                    queue.push(res)
-                } else {
-                    sbuf.appendBuffer(res);
-                }
-                ondata++;
-                console.log(`ondata: ${ondata}`);
-                if (video.paused) {
-                    video.play();
-                }
-
+    let sourceBuffer;
+    let updateend = 0;
+    let queue = [];
+    mediaSource.addEventListener(
+      "sourceopen",
+      function(e) {
+        sourceBuffer = mediaSource.addSourceBuffer('video/webm;codecs="vp8"');
+        sourceBuffer.addEventListener(
+          "updateend",
+          function(e) {
+            if (queue.length > 0 && !sourceBuffer.updating) {
+              sourceBuffer.appendBuffer(queue.shift());
             }
-            fr.readAsArrayBuffer(e.data);
-            allChunks.push(e);
-            
-        }
-        recorder.ondataavailable = (e) => {
-            dataavailable(e);
-        }
+          },
+          true
+        );
+      },
+      true
+    );
 
+    mediaSource.addEventListener("sourceopen", function(e) {
+      console.log("m sourceopen: " + mediaSource.readyState);
+    });
+    mediaSource.addEventListener("sourceended", function(e) {
+      console.log("m sourceended: " + mediaSource.readyState);
+    });
+    mediaSource.addEventListener("sourceclose", function(e) {
+      console.log("m sourceclose: " + mediaSource.readyState);
+    });
+    mediaSource.addEventListener("error", function(e) {
+      console.log("m error: " + mediaSource.readyState);
+    });
 
-        // start stream
-        recorder.start(500);
-        
-        video.autoplay = true;
-        video.src = URL.createObjectURL(mediaSource);
-        console.log('video: ')
-        console.log(video);
-        video.play();
-        console.log(mediaSource.activeSourceBuffers)
-        console.log(mediaSource.readyState)
+    let ondata = 0;
+
+    const video = document.getElementById("video") as HTMLVideoElement;
+
+    video.addEventListener("canplay", e => {
+      video.play();
+      console.log("v can play");
+    });
+
+    video.addEventListener("canplaythrough", e => {
+      video.play();
+      console.log("v can playthrough");
+    });
+    video.addEventListener("loadeddata", e => {
+      console.log("v loaded data");
+    });
+
+    video.addEventListener("play", e => {
+      console.log("v play");
+    });
+
+    video.addEventListener("playing", e => {
+      console.log("v playing");
+    });
+
+    video.addEventListener("stalled", e => {
+      console.log("v stalled");
+    });
+
+    video.addEventListener("timeupdate", e => {
+      console.log("v timeupdate");
+    });
+
+    video.addEventListener("pause", e => {
+      console.log("v pause");
+    });
+
+    video.addEventListener("ended", e => {
+      console.log("v ended");
+    });
+
+    video.addEventListener("waiting", e => {
+      console.log("v waiting");
+    });
+
+    video.addEventListener("complete", e => {
+      console.log("v complete");
+    });
+
+    video.addEventListener("durationchange", e => {
+      console.log("v durationchange");
+    });
+    const mirrorCanvas = e => {
+      new Response(e.data as Blob).arrayBuffer().then(res => {
+        if (video.paused) {
+          video.play();
+        }
+        if (sourceBuffer.updating || queue.length > 0) {
+          console.log("pushed res");
+          queue.push(res);
+        } else {
+          console.log("added source buffer");
+          sourceBuffer.appendBuffer(res);
+        }
+      });
+      //     })
+      //   let fr = new FileReader();
+      //   fr.onloadend = function(e) {
+      //     console.log(e);
+      //     let res = new Uint8Array(fr.result as ArrayBuffer);
+      //     if (sourceBuffer.updating) {
+      //       queue.push(fr.result);
+      //     } else {
+      //       sourceBuffer.appendBuffer(fr.result);
+      //     }
+      //     ondata++;
+      //     console.log(`ondata: ${ondata}`);
+      //     if (video.paused) {
+      //       video.play();
+      //     }
+      //   };
+      //   fr.readAsArrayBuffer(e.data);
+      //   allChunks.push(e);
+      //   console.log(allChunks);
+    };
+
+    const flushevents = e => {
+      console.log("flushing");
+    };
+
+    setInterval(flushevents, 1000);
+
+    el.addEventListener("mouseup", e => {
+      recorder.requestData();
+      recorder.pause();
+    });
+    el.addEventListener("mousedown", e => {
+      recorder.requestData();
+      if (recorder.state === "paused") {
+        recorder.resume();
+      }
+    });
+    recorder.ondataavailable = (e: any) => {
+      console.log("dataavail");
+      mirrorCanvas(e);
+    };
+
+    // setInterval(() => {
+    //   recorder.requestData();
+    // }, 500);
+
+    // start stream
+    recorder.start(120);
+
+    video.autoplay = true;
+    video.src = URL.createObjectURL(mediaSource);
+    console.log("video: ");
+    console.log(video);
+    video.play();
+    console.log(mediaSource.activeSourceBuffers);
+    console.log(mediaSource.readyState);
+  };
+
+  private handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // enter key
+    if (event.keyCode === 13) {
+      this.sendMessage();
     }
+  };
 
-    private handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        // enter key
-        if (event.keyCode === 13) {
-            this.sendMessage();
-        }
-
+  private sendMessage = () => {
+    if (!this.chatInput.current) {
+      return;
     }
-
-    private sendMessage = () => {
-        if (!this.chatInput.current) {
-            return;
-        }
-        const message = this.chatInput.current.value;
-        if (message.length > 0) {
-            this.gameAPI.sendMessage(message);
-            this.chatInput.current.value = '';
-        }
-        this.setState((prev:State) => ({
-            messageHistory: [...prev.messageHistory, {name: this.state.displayName, message}]
-        }));
+    const message = this.chatInput.current.value;
+    if (message.length > 0) {
+      this.gameAPI.sendMessage(message);
+      this.chatInput.current.value = "";
     }
+    this.setState((prev: State) => ({
+      messageHistory: [
+        ...prev.messageHistory,
+        { name: this.state.displayName, message }
+      ]
+    }));
+  };
 
-
-    private onReceiveMessage = (event: MessageEvent) => {
-        console.log(event.data);
-        const _parsed = JSON.parse(event.data);
-        const {name, message} = _parsed;
-        this.setState((prev:State) => ({
-            messageHistory: [...prev.messageHistory, {name, message}]
-        }))
-    }
+  private onReceiveMessage = (event: MessageEvent) => {
+    console.log(event.data);
+    const _parsed = JSON.parse(event.data);
+    const { name, message } = _parsed;
+    this.setState((prev: State) => ({
+      messageHistory: [...prev.messageHistory, { name, message }]
+    }));
+  };
 }
 
 export default GameLobby;
